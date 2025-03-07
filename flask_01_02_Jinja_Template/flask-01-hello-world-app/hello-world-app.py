342726064a8224da2bb24e8e469c730bea1d2ece
aziz <azizeren@yahoo.com>
2025-03-07 13:24:40 +0100

    flask 01-02 added

---
+++ flask_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,26 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route('/')
+def head():
+     return 'Hello World'
+
+
+@app.route('/secondpage')
+def second():
+     return 'This is second page'
+
+
+@app.route('/third')
+def third():
+     return 'This is third page'
+
+
+@app.route('/fourth/<string:id>')
+def fourth(id):
+     return f'id of this page is {id}'
+
+
+if __name__ == '__main__':
+
+     # app.run(debug=True, port=3000)     #flask default 5000 portu
+     app.run(host= '0.0.0.0', port=80)
